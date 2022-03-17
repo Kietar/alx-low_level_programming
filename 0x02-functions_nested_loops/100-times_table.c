8cbe35145a2905b987816db61c6994536395a69d
Kietar <Kenneddykiam@gmail.com>
2022-03-18 01:04:25 +0300

    readme

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,65 +1,48 @@
-#include "main.h"
+#include "holberton.h"
 /**
-* print_times_table - a function that prints the n times table starting from 0
-*
-* @n: number input Ranges from 0-15 (inclusive)
-*
-*/
-void print_number(int n);
+ * print_times_table - print the time tables
+ *@n: the tested number
+ *
+ * Return: the time tables from 0 to n
+ */
 void print_times_table(int n)
 {
-	int a, i, row, col, count;
+	int multi, row, col, cents, tens, units;
 
-	count = 0;
-
-	if (!(n > 15 || n < 0))
+	for (row = 0; row <= n; row++)
 	{
-		for (row = 0; row < n; row++)
+		for (col = 0; col <= n; col++)
 		{
-			_putchar('0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(' ');
-		}
-		_putchar('0');
-		_putchar('\n');
-		for (row = 1; row <= n; row++)
-		{
-			_putchar('0');
-			_putchar(',');
-			for (col = row; col <= n * row; col += row)
+			if (n <= 15 && n >= 0)
 			{
-				a = col;
-				count = 0;
-				do {
-					a /= 10;
-					++count;
-				} while (a != 0);
-				for (i = 0; i < 4 - count; i++)
+				multi = row * col;
+				cents = multi / 100;
+				tens = (multi / 10) % 10;
+				units = multi % 10;
+				if (col == 0)
 				{
-					_putchar(' ');
-				}
+					_putchar('0'); }
+				else
+				{
+					if (multi > 99)
+						_putchar(cents + '0');
+					else
+						_putchar(' ');
+					if (multi > 9)
+						_putchar(tens + '0');
+					else
+						_putchar(' ');
 
-				print_number(col);
-				if (col != n * row)
+					_putchar(units + '0');
+				}
+				if (col < n)
+				{
 					_putchar(',');
+					_putchar(' '); }
+				else
+				{
+					_putchar('\n'); }
 			}
-			_putchar('\n');
 		}
 	}
-}
-
-/**
-* print_times_table - a function that prints the input number digit by digit
-*
-* @n: number input
-*
-*/
-void print_number(int n)
-{
-	if (n / 10)
-	print_number(n / 10);
-
-	_putchar(n % 10 + '0');
 }
