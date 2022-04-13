48adc35003398c06416ef4da6975a1d43d0318a3
Kietar <Kenneddykiam@gmail.com>
2022-04-13 22:25:56 +0300

    readme

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,12 +1,49 @@
-#ifndef FILE_F_POINTRS
-#define FILE_F_POINTRS
-
 #include <stdlib.h>
+#include <stdio.h>
 
-void print_name(char *name, void (*f)(char *));
+/**
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
+void print_opcodes(char *a, int n)
+{
+	int i;
 
-void array_iterator(int *array, size_t size, void (*action)(int));
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
 
-int int_index(int *array, int size, int (*cmp)(int));
+}
 
-#endif
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
+}
